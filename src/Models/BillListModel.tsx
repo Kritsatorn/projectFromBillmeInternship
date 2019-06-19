084a44d404452749d8f10f900262fe38c2261156
Kritsatorn <kritsatorn@billme.co.th>
2019-06-19 18:28:16 +0700

    fixed BillListModel

--- src/Models/BillListModel.tsx
+++ src/Models/BillListModel.tsx
@@ -1,42 +1,44 @@
 export class BillListModel {
-	billList: {
-		billId: string;
-		billName: string;
-		billOwner: string;
-		publishDate: string;
-		image: string;
-	}[];
+  billList: {
+  billId: string;
+  billName: string;
+  billOwner: string;
+  publishDate: string;
+  image: string;
+  billStatus: boolean;
+  }[];
 
-	static apply(datalist: {
-		id: string,
-		name: string,
-		owner_id: string,
-		publish_date: string,
-		status: boolean,
-		image: string
-		}[]
-	) {
-			return new BillListModel(datalist);
-		}
+  static apply(datalist: {
+    id: string,
+    name: string,
+    owner_id: string,
+    publish_date: string,
+    status: boolean,
+    image: string
+  }[]
+  ) {
+  return new BillListModel(datalist);
+  }
 
-	constructor (
-		datalist: {
-		id: string,
-		name: string,
-		owner_id: string,
-		publish_date: string,
-		status: boolean,
-		image: string
-		}[]
-	) {
-			this.billList = datalist.map(data => {
-				return {
-					billId: data.id,
-					billName: data.name,
-					billOwner: data.owner_id,
-					publishDate: data.publish_date,
-					image: data.image
-				};
-			});
-		}
+  constructor (
+    datalist: {
+      id: string,
+      name: string,
+      owner_id: string,
+      publish_date: string,
+      status: boolean,
+      image: string
+    }[]
+  ) {
+  this.billList = datalist.map(data => {
+    return {
+      billId: data.id,
+      billName: data.name,
+      billOwner: data.owner_id,
+      publishDate: data.publish_date,
+      image: data.image,
+      billStatus: data.status
+    };
+  });
+  }
 }
