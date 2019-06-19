1a3358e98995c781d49bffd5e4340a762259e74c
Kritsatorn <kritsatorn@billme.co.th>
2019-06-19 18:29:15 +0700

    fixed MockData

--- src/mocks/BillData.tsx
+++ src/mocks/BillData.tsx
@@ -1,20 +1,20 @@
 export const data = {
-	"quotations": [
-		{
-			"id": "F9961d0b6ead915abb46e03c67c20817",
-			"status": "partial_paid",
-			"name": "ร้านป้าแก้ว",
-			"owner_id": "fffffffffffff0001",
-			"image": "MOCK_URL",
-			"publish_date": "14 DEC 2019"
-		},
-		{
-			"id": "C561d0b6ead915abb46e03c67c20817fa",
-			"status": "partial_paid",
-			"name": "ร้านไข่",
-			"owner_id": "fffffffffffff0001",
-			"image": "MOCK_URL",
-			"publish_date": "14 DEC 2019"
-		}
-	]
-}
+  'quotations': [
+    {
+      id: 'F9961d0b6ead915abb46e03c67c20817',
+      status: false,
+      name: 'ร้านป้าแก้ว',
+      owner_id: 'fffffffffffff0001',
+      image: 'MOCK_URL',
+      publish_date: '14 DEC 2019'
+    },
+    {
+      id: 'C561d0b6ead915abb46e03c67c20817fa',
+      status: false,
+      name: 'ร้านไข่',
+      owner_id: 'fffffffffffff0001',
+      image: 'MOCK_URL',
+      publish_date: '14 DEC 2019'
+    }
+  ]
+};
