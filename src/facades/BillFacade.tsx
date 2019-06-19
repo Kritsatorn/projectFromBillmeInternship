3488a9b2f31a7d4ae87f3cb9ad0a5cd3974ebb86
Kritsatorn <kritsatorn@billme.co.th>
2019-06-19 18:29:28 +0700

    fixed BillFacade

--- src/facades/BillFacade.tsx
+++ src/facades/BillFacade.tsx
@@ -1,13 +1,11 @@
 import { BillListModel } from '../Models/BillListModel';
 import { BillDatasource } from '../datasources/BillDatasource';
-// import { data } from '../mocks/BillData';
 
 export class BillFacade {
-
-	static getBillList(id: string) {
-		return BillDatasource.getBillList(id)
-		.then(result => {
-			return BillListModel.apply(result);
-		})
-	}
+  static getBillList(id: string) {
+    return BillDatasource.getBillList(id)
+    .then(result => {
+      return BillListModel.apply(result);
+    });
+  }
 }
