6dc2b68f331c02452ca11bbfe14d85afcc1dd66d
RawitSHIE <Rawitgun@gmail.com>
2019-06-19 16:11:38 +0700

    created checkbox component

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { CheckboxProps } from './CheckboxType';
+import './Checkbox.css';
+
+export class Checkbox extends React.Component<CheckboxProps> {
+  render() {
+    const{title, checked} = this.props;
+
+    return (
+      <div>
+        <label className="Container">
+          <input
+            type="checkbox"
+            className="Checkbox__input"
+            checked={checked}
+          />
+        <span className="Text-label">{title}</span>
+        </label>
+      </div>
+    );
+  }
+}
