6cc81e417c9ce521867d80abaa15f23fd84e2b09
Kritsatorn <kritsatorn@billme.co.th>
2019-06-19 16:10:38 +0700

    created BillModel

---
+++ src/Models/BillListModel.tsx
@@ -0,0 +1,42 @@
+export class BillListModel {
+	billList: {
+		billId: string;
+		billName: string;
+		billOwner: string;
+		publishDate: string;
+		image: string;
+	}[];
+
+	static apply(datalist: {
+		id: string,
+		name: string,
+		owner_id: string,
+		publish_date: string,
+		status: boolean,
+		image: string
+		}[]
+	) {
+			return new BillListModel(datalist);
+		}
+
+	constructor (
+		datalist: {
+		id: string,
+		name: string,
+		owner_id: string,
+		publish_date: string,
+		status: boolean,
+		image: string
+		}[]
+	) {
+			this.billList = datalist.map(data => {
+				return {
+					billId: data.id,
+					billName: data.name,
+					billOwner: data.owner_id,
+					publishDate: data.publish_date,
+					image: data.image
+				};
+			});
+		}
+}
