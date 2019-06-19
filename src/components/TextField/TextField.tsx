7ee12ad2610f928de8679f96428a46385f4a4f5b
RawitSHIE <Rawitgun@gmail.com>
2019-06-19 18:38:47 +0700

    reindentation code

--- src/components/TextField/TextField.tsx
+++ src/components/TextField/TextField.tsx
@@ -5,27 +5,27 @@ import './TextField.css';
 export class TextField
   extends React.Component<TextFieldProps> {
 
-    render() {
-      const {
-        name, id, disabled,
-        required, placeHolder,
-        type
-      } = this.props;
+  render() {
+    const {
+      name, id, disabled,
+      required, placeHolder,
+      type
+    } = this.props;
 
-      return (
-          <input
-            name={name}
-            id={id}
-            type={type}
-            className="text-field__input"
-            required={required}
-            placeholder={placeHolder}
-            disabled={disabled}
-          />
-      );
-
-    }
-    stylingField(className: string) {
-      return 'Text-field ' + className;
-    }
+    return (
+      <input
+        name={name}
+        id={id}
+        type={type}
+        className="text-field__input"
+        required={required}
+        placeholder={placeHolder}
+        disabled={disabled}
+      />
+    );
   }
+
+  stylingField(className: string) {
+    return 'Text-field ' + className;
+  }
+}
