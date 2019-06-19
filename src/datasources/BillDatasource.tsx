056f0de6fc1c512706fa7e807522ee94cf58dad1
Kritsatorn <kritsatorn@billme.co.th>
2019-06-19 18:29:02 +0700

    fixed BillDatasource

--- src/datasources/BillDatasource.tsx
+++ src/datasources/BillDatasource.tsx
@@ -1,11 +1,8 @@
-import { API } from '../utilities/API';
+// import { API } from '../utilities/API';
+import { data } from '../mocks/BillData';
 
-const securityUrl = '';
 export class BillDatasource {
-	static getBillList(id: string) {
-		return  API.get(securityUrl, `/link/eiei/${id}`)
-		.catch(() => {
-			return 0
-		});
-	}
+  static getBillList(id: string) {
+    return Promise.resolve(data.quotations);
+  }
 }
