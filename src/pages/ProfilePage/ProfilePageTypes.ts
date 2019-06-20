4ce3791f117da4d435fa111c2966006dd2a21a9d
Kritsatorn <kritsatorn@billme.co.th>
2019-06-20 15:53:03 +0700

    fixed indent

--- src/pages/ProfilePage/ProfilePageTypes.ts
+++ src/pages/ProfilePage/ProfilePageTypes.ts
@@ -1,12 +1,12 @@
 export interface ProfilePageState {
   userId: string;
   billInfo: {
-  billId: string;
-  billName: string ;
-  billOwner: string;
-  billStatus: boolean;
-  publishDate: string;
-  image: string;
+    billId: string;
+    billName: string ;
+    billOwner: string;
+    billStatus: boolean;
+    publishDate: string;
+    image: string;
   }[];
   isLoadingComplete: boolean;
 }
