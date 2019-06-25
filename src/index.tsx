0ee9508ca5d3a3759940e61ce99b3b9a2dcadda8
RawitSHIE <Rawitgun@gmail.com>
2019-06-25 14:00:49 +0700

    added addfriendpage path

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import { createBrowserHistory } from 'history';
 import { Route, Switch, Router } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { ProfilePage } from './pages/ProfilePage/ProfilePage';
+import { AddFriendPage } from './pages/AddFriendPage/AddFriendPage';
 import { CreateBillPage } from './pages/CreateBillPage/CreateBillPage';
 import './styles/index.css';
 
@@ -14,6 +15,7 @@ ReactDOM.render(
     <Switch>
       <Route exact={true} path="/" component={ProfilePage} />
       <Route exact={true} path="/create" component={CreateBillPage}/>
+      <Route exact={true} path="/add" component={AddFriendPage}/>
     </Switch>
   </Router>,
   document.getElementById('root')
