469ee152703e9da1885c0a554225ece00af96dcb
Kritsatorn <kritsatorn@billme.co.th>
2019-06-25 15:53:51 +0700

    fixed id

--- src/datasources/BillDatasource.tsx
+++ src/datasources/BillDatasource.tsx
@@ -4,7 +4,7 @@ export class BillDatasource {
   static getBillList(id: string) {
     return API.get(
       'https://dev-kidtang.billme.co.th/services',
-      `/api/line/users/Uf77cf2a5c273a986359267abea8fe35e/quotations`
+      `/api/line/users/${id}/quotations`
     );
   }
 }
