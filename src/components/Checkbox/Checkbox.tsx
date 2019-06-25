a3760026c917150b92d0b77caea98873796e9f95
Supachai  Chankate <bank.chankate@gmail.com>
2019-06-25 06:33:31 +0000

    Update Checkbox.tsx

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { CheckboxProps } from './CheckboxTypes';
 import './Checkbox.css';
 
-export class Checkbox extends React.Component<CheckboxProps> {
+export class Checkbox
+    extends React.Component<CheckboxProps> {
 
   render() {
     const{title, checked, onChange} = this.props;
