b0a65f2832ff22747f9827114fcf393893501b6a
RawitSHIE <Rawitgun@gmail.com>
2019-06-27 11:44:15 +0700

    added billing state type to select friend page

--- src/pages/SelectFriendPage/SelectFriendPageTypes.ts
+++ src/pages/SelectFriendPage/SelectFriendPageTypes.ts
@@ -3,11 +3,24 @@ export interface SelectFriendState {
   friends: Friend[];
   selectedFriendList: Friend[];
   selectedFriend: number;
+  totalPrice: number;
+  items: Item[];
+  vat: number;
+  vatStatus: boolean;
+  vatPrice: number;
   totalBillPrice: number;
+  serviceCharge: number;
+  serviceChargeStatus: boolean;
+  serviceChargePrice: number;
 }
 
 export interface Friend {
   profilePic: string;
   displayName: string;
   isSelect: boolean;
+}
+
+export interface Item {
+  detail: string;
+  price: number;
 }
