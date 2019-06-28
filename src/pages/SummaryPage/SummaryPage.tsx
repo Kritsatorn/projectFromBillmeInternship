f15c95218ba54e20b62023a2d9ca5d06a7da1422
Kritsatorn <kritsatorn@billme.co.th>
2019-06-28 11:20:48 +0700

    added base structure

---
+++ src/pages/SummaryPage/SummaryPage.tsx
@@ -0,0 +1,87 @@
+import * as React from 'react';
+import { SummaryPageState } from './SummaryPageTypes';
+import './SummaryPage.css';
+
+export class SummaryPage
+  extends React.Component<object, SummaryPageState> {
+
+    constructor(props: object) {
+      super(props);
+
+      this.state = {
+        showFriendMenu: false ,
+        bill: {
+          billImage: '',
+          billName: 'ค่าข้าวเที่ยงร้านเฮลโล่ว',
+          billDate: '18 มิ.ย. 2562',
+          billOwnerName: 'Tangkwa',
+          billOwnerImage: 'https://bit.ly/2J9C0Hv'
+        },
+        friends: [
+          {
+            userId: '2',
+            profilePic: 'https://bit.ly/2J9C0Hv',
+            displayName: 'Wiput',
+            owner: false
+          },
+          {
+            userId: '2',
+            profilePic: 'https://bit.ly/2J9C0Hv',
+            displayName: 'Wiput',
+            owner: false
+          },
+          {
+            userId: '2',
+            profilePic: 'https://bit.ly/2J9C0Hv',
+            displayName: 'Wiput',
+            owner: false
+          },
+          {
+            userId: '2',
+            profilePic: 'https://bit.ly/2J9C0Hv',
+            displayName: 'Wiput',
+            owner: false
+          },
+        ]
+      };
+    }
+
+    render() {
+      return (
+        <div className="summary__background">
+          <div className="header__background">
+            <img className="bill-image" src={require('../../assets/upload__picture.jpg')}/>
+            <div className="bill-detail">
+              <div className="bill-name">{this.state.bill.billName}</div>
+              <div className="bill-date">{this.state.bill.billDate}</div>
+              <div className="bill-user">
+                <img className="bill-owner-image" src={this.state.bill.billOwnerImage}/>
+                <div className="bill-owner-name">{this.state.bill.billOwnerName}</div>
+              </div>
+            </div>
+          </div>
+          <div className="friend-feild">
+              <div className="friend-bar">
+                <div className="friend-bar-title">เพื่อนที่หารด้วยกัน</div>
+                <div className="friend-bar-button">b</div>
+              </div>
+              <div className="friend-show">
+                {
+                  this.state.showFriendMenu ?
+                  null :
+                  null
+                }
+              </div>
+          </div>
+        </div>
+      );
+    }
+
+    renderFriendRow() {
+      return(
+        <div>
+          hi
+        </div>
+      );
+    }
+  }
---
+++ src/pages/SummaryPage/SummaryPageTypes.ts
@@ -0,0 +1,21 @@
+export interface SummaryPageState {
+  text?: string;
+  bill: Bill;
+  showFriendMenu: boolean;
+  friends: Friend[];
+}
+
+export interface Bill {
+  billImage: string;
+  billName: string;
+  billDate: string;
+  billOwnerName: string;
+  billOwnerImage: string;
+}
+
+export interface Friend {
+  userId: string;
+  profilePic: string;
+  displayName: string;
+  owner: boolean;
+}
