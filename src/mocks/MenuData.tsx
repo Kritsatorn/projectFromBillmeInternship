07f70fc46f654a8ac816ff03d3a577bb5ff19d22
Kritsatorn <kritsatorn@billme.co.th>
2019-07-01 18:20:48 +0700

    added mock data

---
+++ src/mocks/MenuData.tsx
@@ -0,0 +1,24 @@
+export const MenuData = {
+  menu: [
+    {
+      menuName: 'แกงส้มไข่ชะอม',
+      menuPrice: '55.0'
+    },
+    {
+      menuName: 'แกงส้มไข่ชะอม',
+      menuPrice: '55.0'
+    },
+    {
+      menuName: 'แกงส้มไข่ชะอม',
+      menuPrice: '55.0'
+    },
+    {
+      menuName: 'แกงส้มไข่ชะอม',
+      menuPrice: '55.0'
+    },
+    {
+      menuName: 'แกงส้มไข่ชะอม',
+      menuPrice: '55.0'
+    }
+  ]
+};
